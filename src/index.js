13fd770de3e3575ca0ab4ce806fc2ef03c1d98a7
Helena Belicheva <bbelht@gmail.com>
2022-01-08 05:44:00 +0500

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+  var temp, reverseInt = 0;
   
+  while (n) {
+    temp = n % 10;
+    reverseInt = (reverseInt * 10) + temp;
+    n = parseInt(n / 10);
+  }
+  if (reverseInt < 0){
+      reverseInt = reverseInt * -1;
+  }
+  return reverseInt;
 }
